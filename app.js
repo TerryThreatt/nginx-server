34ff99ed9fc875628b169d941fa77bc7c09fb606
TerryThreatt <terry.threatt@gmail.com>
2021-04-19 21:40:45 +0000

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000 
 
 app.get('/', (req, res) => {
-	res.send('Hello World')
+	res.send('Hello World - check me out')
 })  
 
 app.listen(port, () => console.log(`New app listening on port ${port}`)) 
